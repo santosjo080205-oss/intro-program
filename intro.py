e6caf368e2d6503c79636db6aa9686f75667aeb9
santosjo080205-oss <santosjo080205@gmail.com>
2025-09-05 15:48:40 +0800

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,13 @@
+# Program: my personal introduction
+
+# input section
+full_name = input("Enter your Full Name: ")
+course_yr_sec = input("Enter your Course, Yr. & Sec.: ")
+birthday = input("Enter your Birthday: ")
+motto = input("Enter your Motto/Motivation in Life: ")
+
+# Output section
+print(f"\nHi! I'm {full_name} of {course_yr_sec}. Welcome to Data Structures and Algorithm!")
+print(f"My Birthday is on {birthday}. and my motto/motivation in life is {motto}")
+
+# DONE
